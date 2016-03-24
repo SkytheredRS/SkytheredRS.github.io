e9d81b13ca8b6cd837ab4269d6b2edbcebdfb194
Marco Wong <marco.skybutter@gmail.com>
2016-03-24 13:11:11 +0800

    ahsd

---
+++ jhesdfdshfdjhjf 2/README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/SkytheredRS/pen/mPmLYW.
+
+ 
+
+Forked from [bryant chan](http://codepen.io/BetaRageOP/)'s Pen [GZmqOb](http://codepen.io/BetaRageOP/pen/GZmqOb/).
---
+++ jhesdfdshfdjhjf 2/js/index.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  //create score variable
+  var score = 0;
+  
+  //generate cat every 1 sec
+  setInterval(function(){
+    $("#nyancat").toggleClass("moveCat");
+  }, 3000);
+ 
+  //click to hide cat
+   $("#nyancat").click(function() {
+    $("#nyancat").hide();
+ 
+     //add one to score
+     score += 20;
+     
+   setTimeout(function() {
+     $("#nyancat").show();
+                 }, 1000);
+     });
+ 
+  //generate lol every 1 sec
+  setInterval(function(){
+    $("#lol").toggleClass("moveLol");
+  }, 3000);
+ 
+  //click to hide & wait 1 sec, show
+   $("#lol").click(function() {
+    $("#lol").hide();
+ 
+    //add one to score
+     score += 10;
+     
+   setTimeout(function() {
+     $("#lol").show();
+                 }, 1000);
+     });
+
+  //check score every half second
+  setInterval(function(){
+    $("#score").html(score);
+  }, 500);
+  
+  setTimeout(function(){ 
+    window.location.href=""; 
+  }, 180000);
+
+});
